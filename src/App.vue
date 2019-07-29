663dfe8c237c54e72f2a1d11e47adccddd94a9aa
tdnicola <tdnicola@gmail.com>
2019-07-28 21:12:54 -0400

    added deleting of a todo item

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@
       <input v-model='currentTodo' @keydown.enter='addTodo()' placeholder='Add a todo'>
       <ul class='todo'>
         <li v-for='todo in todos' :key='todo.id'>
-          {{todo.label}}
+          <input type='checkbox'>
+            {{todo.label}}
+            <button @click="removeTodo(todo)">Delete</button>
         </li>
       </ul>
   </div>
@@ -16,14 +18,21 @@ export default {
   data() {
     return {
       todos: [],
-      currentTodo: 'ß'
+      currentTodo: ''
     }
   },
   methods: {
     addTodo() {
       this.todos.push({id:this.todos.length, label: this.currentTodo, completed: false});
       this.currentTodo = '';
-    }
+    },
+    removeTodo(todo) {
+      var index = this.todos.indexOf(todo)
+      this.todos.splice(index,1)
+    },
+    // completeTodo(todo) {
+    //   this.todos
+    // }
   }
 }
 </script>
